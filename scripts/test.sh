d1022f5bd300107643b6af8f94a018ec626083f7
Benjamin Kearns <benjamin.kearns@gmail.com>
2023-05-20 20:36:11 +1200

    Run test scripts from the correct directory

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,11 +2,16 @@
 
 # from: https://medium.com/@taowen/go-test-coverage-for-multi-package-project-d4f36f2b573a
 
+# this assumes it's being run from the build directory
+
 set -e
 echo "" > coverage.txt
 
+ln -s $PWD/proximitypythoninterpreter ../internal/scripting/
+ln -s $PWD/python310 ../internal/scripting/
+
 for d in $(go list ../... | grep -v vendor); do
-    if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then    
         CGO_CFLAGS=-Wno-undef-prefix CGO_ENABLED=1 GOOS=darwin go test -coverprofile=profile.out -coverpkg=github.com/pipeline/proximity-core,github.com/pipeline/proximity-core/pkg/project,github.com/pipeline/proximity-core/internal/proxy,github.com/pipeline/proximity-core/internal/request_queue,github.com/pipeline/proximity-core/internal/scripting $d
     else
         go test -coverprofile=profile.out -coverpkg=github.com/pipeline/proximity-core,github.com/pipeline/proximity-core/pkg/project,github.com/pipeline/proximity-core/internal/proxy,github.com/pipeline/proximity-core/internal/request_queue,github.com/pipeline/proximity-core/internal/scripting $d
@@ -17,3 +22,6 @@ for d in $(go list ../... | grep -v vendor); do
         rm profile.out
     fi
 done
+
+rm ../internal/scripting/proximitypythoninterpreter
+rm ../internal/scripting/python310
